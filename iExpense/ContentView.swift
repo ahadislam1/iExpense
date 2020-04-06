f3d8a2ae146b258b04f9e3b70ab850455be8f308
Ahad Islam <ahad.islam624@gmail.com>
2020-04-06 16:38:06 -0400

    demoed out environment, picker and stuff

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -111,7 +111,7 @@ struct JSONCodingView: View {
     }
 }
 
-struct ExpenseItem: Identifiable {
+struct ExpenseItem: Codable, Identifiable {
     let id = UUID()
     let name: String
     let type: String
@@ -119,29 +119,64 @@ struct ExpenseItem: Identifiable {
 }
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            
+            let decoder = JSONDecoder()
+            
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        
+        self.items = []
+        
+    }
 }
 
 struct ContentView: View {
     @ObservedObject var expenses = Expenses()
+    @State private var showingAddExpense = false
     
     var body: some View {
         NavigationView {
             List {
                 ForEach(expenses.items) { item in
-                    Text(item.name)
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        Text("$\(item.amount)")
+                    }
                 }
-            .onDelete(perform: removeItems(at:))
+                .onDelete(perform: removeItems(at:))
             }
             .navigationBarTitle("iExpense")
             .navigationBarItems(trailing:
                 Button(action: {
-                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                    self.expenses.items.append(expense)
+                    self.showingAddExpense = true
                 }) {
                     Image(systemName: "plus")
                 }
             )
+        }
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: self.expenses)
         }
     }
     
